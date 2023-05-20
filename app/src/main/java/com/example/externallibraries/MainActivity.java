6f9ac02f78964e28e5c13cb161198b4dbc61bf1e
f22andbe <f22andbe@student.his.se>
2023-05-20 21:34:46 +0200

    Add code from project five for reuse here

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -1,14 +1,100 @@
-package com.example.externallibraries;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.networking;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+@SuppressWarnings("FieldCanBeLocal")
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
+    //private final String JSON_FILE = "mountains.json";
+    private String mountainsJSON;
+    private ArrayList<Mountain> mountainArrayList;
+    private Gson gson;
+    RecyclerView recyclerView;
+    Type type;
+    MountainViewAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mountainArrayList = new ArrayList<Mountain>();
+
+        /* Read in recyclerview */
+        recyclerView = findViewById(R.id.recyclerView);
+
+        /* Set up adapter */
+        mAdapter = new MountainViewAdapter(this, mountainArrayList);
+        recyclerView.setAdapter(mAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        /* setup JSON parser */
+        gson = new Gson();
+        type = new TypeToken<ArrayList<Mountain>>() {}.getType();
+
     }
+
+
+    /* parse JSON data and update view */
+    @Override
+    public void onPostExecute(String json) {
+        /* parse json into Mountain objects */
+        //mountainArrayList.clear();
+        mountainArrayList.addAll(gson.fromJson(json, type));
+        //Log.d("onPostExecute(): ", mountainArrayList.toString());
+        mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        /* start JsonTask and fetch data */
+        if (id == R.id.action_get_data) {
+            Log.d("onOptionItemSelected","Get JSON data from web");
+            /* get JSON data from webservice */
+
+            new JsonTask(this).execute(JSON_URL);
+            return true;
+        }
+
+        /* clear data and RecyclerView */
+        if (id == R.id.action_clear_data) {
+            Log.d("onOptionItemSelected","clear data");
+            /* clear data */
+            mountainArrayList.clear();
+            mAdapter.notifyDataSetChanged();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
---
+++ app/src/main/java/com/example/externallibraries/MountainViewAdapter.java
@@ -0,0 +1,68 @@
+package com.example.externallibraries;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MountainViewAdapter extends RecyclerView.Adapter<MountainViewAdapter.MountainViewHolder> {
+
+    Context context;
+    ArrayList<Mountain> mountainArrayList;
+
+    public MountainViewAdapter(Context context, ArrayList<Mountain> mountainArrayList) {
+        this.context = context;
+        this.mountainArrayList = mountainArrayList;
+    }
+
+    @Override
+    public MountainViewAdapter.MountainViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.recycler_layout, parent, false);
+
+        return new MountainViewAdapter.MountainViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MountainViewHolder holder, int position) {
+        holder.mountainName.setText("Name: " + mountainArrayList.get(position).getName());
+        holder.mountainLocation.setText("Location: " + mountainArrayList.get(position).getLocation());
+        holder.mountainHeight.setText("Height: " + mountainArrayList.get(position).getHeight() );
+        holder.mountainWiki.setText("Wiki: " + mountainArrayList.get(position).auxdata.getWiki());
+
+        if((position % 2) == 0) {
+            holder.itemView.setBackgroundColor(context.getResources().getColor(R.color.lightgrey));
+        }else{
+            holder.itemView.setBackgroundColor(context.getResources().getColor(R.color.lightblue));
+        }
+
+    }
+
+    public int getItemCount() {
+        return mountainArrayList.size();
+    }
+
+    public static class MountainViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mountainName;
+        TextView mountainLocation;
+        TextView mountainHeight;
+        TextView mountainWiki;
+
+        public MountainViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mountainName = itemView.findViewById(R.id.mountainname);
+            mountainLocation = itemView.findViewById(R.id.mountainlocation);
+            mountainHeight = itemView.findViewById(R.id.mountainheight);
+            mountainWiki = itemView.findViewById(R.id.mountainwiki);
+        }
+    }
+
+
+}
