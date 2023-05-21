225ce15d17a3d08f0cb463b2427f435af087cbab
f22andbe <f22andbe@student.his.se>
2023-05-22 00:59:36 +0200

    Add logo to about screen

--- app/src/main/java/com/example/externallibraries/AboutActivity.java
+++ app/src/main/java/com/example/externallibraries/AboutActivity.java
@@ -1,6 +1,7 @@
 package com.example.externallibraries;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,14 +9,18 @@ import androidx.appcompat.app.AppCompatActivity;
 public class AboutActivity extends AppCompatActivity {
 
     private TextView textView;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        imageView = findViewById(R.id.logoimage);
         textView = findViewById(R.id.textView);
 
+        // Set image for the about View
+        imageView.setImageResource(R.drawable.logo);
         // Set the text for the TextView
         textView.setText(R.string.about_string);
     }
